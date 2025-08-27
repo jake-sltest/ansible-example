eb6fcbc8f4f449fbbb7e7c88539aa3c30dab37e9
Jake <jacobj@spacelift.io>
2025-08-27 12:32:00 -0700

    fixed rg value

--- admin/main.tf
+++ admin/main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+data "azurerm_resource_group" "rg-winrm-demo" {
+  name = "rg-winrm-demo"
+}
+
 module "azure-worker" {
   source = "github.com/spacelift-io/terraform-azure-spacelift-workerpool?ref=v1.0.0"
 
@@ -17,7 +21,7 @@ module "azure-worker" {
     export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
   EOT
 
-  resource_group = rg-winrm-demo.name # An azurerm_resource_group object - must have `name` and `location` properties
+  resource_group = data.azurerm_resource_group.rg-winrm-demo # An azurerm_resource_group object - must have `name` and `location` properties
   subnet_id      = "/subscriptions/d2d840cc-eb24-4500-a29f-6cddefb542a4/resourceGroups/rg-winrm-demo/providers/Microsoft.Network/virtualNetworks/vnet-winrm-demo/subnets/subnet-winrm-demo"
   worker_pool_id = "01K3PDK560Q2R75KGZN22C4C7Z"
 }
