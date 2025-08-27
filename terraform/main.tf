a575db2f24842a5a1c72ae7ed55b85239c52c807
Jake <jacobj@spacelift.io>
2025-08-27 12:51:15 -0700

    add private ip output

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,6 +160,11 @@ output "winvm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
 
+output "winvm_private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
+
+
 output "ansible_inventory" {
   value = <<EOT
 [windows]
