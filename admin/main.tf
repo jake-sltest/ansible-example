66014f86919658776724dbada6c4c2f604feae7c
Jake <jacobj@spacelift.io>
2025-08-27 12:01:31 -0700

    add workerpool in azure

---
+++ admin/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.61.0"
+    }
+  }
+}
+
+module "azure-worker" {
+  source = "github.com/spacelift-io/terraform-azure-spacelift-workerpool?ref=v1.0.0"
+
+  admin_password = "Super Secret Password!"
+
+  configuration = <<-EOT
+    export SPACELIFT_TOKEN="${var.worker_pool_config}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+  EOT
+
+  resource_group = "/subscriptions/d2d840cc-eb24-4500-a29f-6cddefb542a4/resourceGroups/rg-winrm-demo" # An azurerm_resource_group object - must have `name` and `location` properties
+  subnet_id      = "/subscriptions/d2d840cc-eb24-4500-a29f-6cddefb542a4/resourceGroups/rg-winrm-demo/providers/Microsoft.Network/virtualNetworks/vnet-winrm-demo/subnets/subnet-winrm-demo"
+  worker_pool_id = "01K3PDK560Q2R75KGZN22C4C7Z"
+}
