b364d031e1a083f41da4890a50c0790d049e5463
Jake <jacobj@spacelift.io>
2025-08-27 12:37:18 -0700

    fix

--- admin/main.tf
+++ admin/main.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.61.0"
-      features {}
+      source   = "hashicorp/azurerm"
+      version  = "=3.61.0"
+      features = {}
     }
   }
 }
