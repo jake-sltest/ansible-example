cb151a7f464c51894c7b406f3141604cdfcc00f1
Jake <jacobj@spacelift.io>
2025-08-27 17:24:29 -0700

    allow rdp

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,21 @@ resource "azurerm_network_security_rule" "winrm_https" {
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "Allow-RDP"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*" # Ideally your IP/subnet
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+
 #############################
 # Public IP
 #############################
