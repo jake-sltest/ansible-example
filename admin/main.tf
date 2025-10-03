fcbd45fa3f5485a910f823f49ad325d5509ac1af
jacobjSL <118396550+jacobjSL@users.noreply.github.com>
2025-10-03 12:41:06 -0700

    Update main.tf

--- admin/main.tf
+++ admin/main.tf
@@ -18,5 +18,5 @@ module "azure-worker" {
 
   resource_group = data.azurerm_resource_group.rg-winrm-demo # An azurerm_resource_group object - must have `name` and `location` properties
   subnet_id      = "/subscriptions/d2d840cc-eb24-4500-a29f-6cddefb542a4/resourceGroups/rg-winrm-demo/providers/Microsoft.Network/virtualNetworks/vnet-winrm-demo/subnets/subnet-winrm-demo"
-  worker_pool_id = "01K3PDK560Q2R75KGZN22C4C7Z"
+  worker_pool_id = var.worker_pool_id
 }
